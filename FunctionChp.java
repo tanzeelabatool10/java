68f58e101dbb50fdc5dbce661b8b8b4f02e31b89
Tanzeela Batool <tanzeelabatoolumw@gmail.com>
2026-03-01 22:20:53 -0800

    basic use of function.

---
+++ FunctionChp.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class FunctionChp {
+
+
+    public static void printHello() {
+        System.out.println("Hello, World!");
+    }
+
+    public static int calculateSum(int num1, int num2) {
+        
+        int sum = num1 + num2;
+        return sum;
+    }
+    public static void main(String[] args) {
+        printHello();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int sum = calculateSum(num1, num2);
+        System.out.println("sum is : " + sum);
+        
+
+
+
+    }
+
+    
+}
