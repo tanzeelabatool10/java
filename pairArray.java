2feb0b1eb7285c3d79653f2704e69eea68f29d85
Tanzeela Batool <tanzeelabatoolumw@gmail.com>
2026-03-10 07:16:55 -0700

    practicing array

--- pairArray.java
+++ pairArray.java
@@ -3,13 +3,17 @@ import java.util.*;
 public class pairArray {
 
     public static void printPair(int arr[]){
+        // Total pairs
+        int totalPairs =0;
         for(int i=0; i<arr.length; i++){
             int curr = arr[i]; //1,2,3,4,5
             for(int j=i+1; j<arr.length; j++){
                 System.out.print("("+curr+","+arr[j]+")");
+                totalPairs++;
             }
             System.out.println();
         }
+        System.out.println("Total Pairs: "+totalPairs);
     }
 
     public static void main(String args[]){
